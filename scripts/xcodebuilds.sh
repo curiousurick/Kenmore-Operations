d5cb525888d95e653dcedd873fa6dab46dc5073e
curiousurick@icloud.com <curiousurick@icloud.com>
2024-10-14 16:33:22 -0700

    Updating script to 18.1

--- scripts/xcodebuilds.sh
+++ scripts/xcodebuilds.sh
@@ -5,6 +5,6 @@ base_path=$(git rev-parse --show-toplevel);
 
 set -e
 
-xcodebuild test -project "${base_path}/Kenmore-Operations.xcodeproj" -scheme Kenmore-Operations -destination 'platform=tvOS Simulator,name=Apple TV 4K (3rd generation),OS=16.1';
+xcodebuild test -project "${base_path}/Kenmore-Operations.xcodeproj" -scheme Kenmore-Operations -destination 'platform=tvOS Simulator,name=Apple TV 4K (3rd generation),OS=18.1';
 
 exit 0
